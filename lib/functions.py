ee96a2da13b1f1ddab13d8b0ec6368d3f2a7d86a
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-08-15 13:01:51 +0300

    complete assignment

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,28 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
+greet_programmer()
 
 def greet(name):
     pass
+    print(f"Hello, {name}!")
+greet("Naureen")
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
+greet_with_default()
+greet_with_default("Sunny")
 
 def add(num1, num2):
     pass
+    return num1 + num2
+print(add(1,2))
 
 def halve(number):
     pass
+    # if "number" != int: return None
+    return number/2
+print(halve(4))
+    
